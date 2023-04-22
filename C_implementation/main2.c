085b4d478fb6f63ad0d8294043cca7de089b4946
Nirvana <thatonebipanda@gmail.com>
2023-04-22 09:14:51 +0530

    eh

--- C_implementation/main2.c
+++ C_implementation/main2.c
@@ -3,19 +3,16 @@
 #include <openssl/sha.h>
 
 int main() {
-    char password[100];
-    printf("Enter your password: ");
-    fgets(password, 100, stdin);
-    password[strcspn(password, "\n")] = '\0'; // remove newline character from input
-    
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)password, strlen(password), hash);
-    
-    printf("Hashed value of '%s': ", password);
+    unsigned char *input_data = "Hello, world!";
+    unsigned char hash_value[SHA256_DIGEST_LENGTH];
+
+    SHA256(input_data, strlen(input_data), hash_value);
+
+    printf("SHA-256 hash of \"%s\":\n", input_data);
     for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        printf("%02x", hash[i]);
+        printf("%02x", hash_value[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
---
+++ C_implementation/prod.c
@@ -0,0 +1,11 @@
+//todo build a login system with a database
+//todo 1. store passwords in a way that is not plaintext
+//todo 2. allow users to log in without storing their passwords in plaintext
+//todo 3. rate limit login attempts
+
+#include {stdio.h}
+#include {stdlib.h}
+#include {string.h}
+#include {unistd.h}
+#include {stdbool.h}
+#include {time.h}
